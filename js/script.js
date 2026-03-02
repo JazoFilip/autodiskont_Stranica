47d6e5af3645944d280800a6c9f352b9554be2af
Filip <filip.jazo29@gmail.com>
2026-03-02 14:24:40 +0100

    1st launch site

--- js/script.js
+++ js/script.js
@@ -20,5 +20,8 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 
 hamburger.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+  navLinks.classList.toggle("active");
+
+  const expanded = hamburger.getAttribute("aria-expanded") === "true";
+  hamburger.setAttribute("aria-expanded", !expanded);
 });
